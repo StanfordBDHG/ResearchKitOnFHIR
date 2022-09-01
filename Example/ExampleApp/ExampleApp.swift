edd41cf9f6a722763ca7c38b2c221853e09614fc
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-09-01 15:12:21 -0400

    Reorganize files

--- Example/ExampleApp/ExampleApp.swift
+++ Example/ExampleApp/ExampleApp.swift
@@ -13,53 +13,11 @@ import SwiftUI
 
 @main
 struct ExampleApp: App {
-    @State private var presentQuestionnaire = false
-    @State private var activeQuestionnaire: Questionnaire?
-
-    private let exampleQuestionnaires: [Questionnaire] = [
-        .skipLogicExample,
-        .textValidationExample
-    ]
-    
     var body: some Scene {
         WindowGroup {
-            List {
-                Section{
-                    ForEach(exampleQuestionnaires, id: \.self) { questionnaire in
-                        Button(questionnaire.title?.value?.string ?? "Untitled Questionnaire") {
-                            activeQuestionnaire = questionnaire
-                            presentQuestionnaire = true
-                        }
-                    }
-                } header: {
-                    Text("Example FHIR Questionnaires")
-                }
-            }
-            .sheet(isPresented: $presentQuestionnaire) {
-                QuestionnaireView(questionnaire: self.$activeQuestionnaire)
-            }
+            QuestionnaireListView()
         }
     }
 }
 
-struct QuestionnaireView: View {
-    @Binding var questionnaire: Questionnaire?
 
-    func createTask(questionnaire: Questionnaire) -> ORKNavigableOrderedTask? {
-        do {
-            return try ORKNavigableOrderedTask(questionnaire: questionnaire)
-        } catch {
-            print("Error creating task: \(error)")
-        }
-        return nil
-    }
-
-    var body: some View {
-        if let activeQuestionnaire = questionnaire,
-           let task = createTask(questionnaire: activeQuestionnaire) {
-            ORKOrderedTaskView(tasks: task)
-        } else {
-            Text("ERROR_MESSAGE")
-        }
-    }
-}
