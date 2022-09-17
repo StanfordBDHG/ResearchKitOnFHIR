fcfb9c86861a1659fd91c6b33a17caf46e518f45
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-09-17 09:05:49 -0400

    Add UI Tests

--- Example/ExampleUITests/ExampleUITests.swift
+++ Example/ExampleUITests/ExampleUITests.swift
@@ -29,7 +29,7 @@ final class ExampleUITests: XCTestCase {
         app.swipeDown(velocity: XCUIGestureVelocity.fast)
     }
     
-    func testQuestionnaireResults() throws {
+    func testSkipLogicExample() throws {
         let app = XCUIApplication()
         app.launch()
         
@@ -69,5 +69,108 @@ final class ExampleUITests: XCTestCase {
         
         // Dismiss results view
         app.swipeDown(velocity: XCUIGestureVelocity.fast)
+    }
+
+    func testContainedValueSetExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let containedValueSetExampleButton = app.collectionViews.buttons["Contained ValueSet Example"]
+
+        // Complete questionnaire
+        containedValueSetExampleButton.tap()
+        app.tables.staticTexts["Yes"].tap()
+        app.buttons["Done"].tap()
+
+        // Open context menu and view results
+        containedValueSetExampleButton.press(forDuration: 1.0)
+        app.collectionViews.buttons["View Responses"].tap()
+
+        // Check results
+        let buttonsInResultView = app.collectionViews.allElementsBoundByIndex[1].buttons
+        XCTAssertEqual(buttonsInResultView.count, 1)
+        buttonsInResultView.allElementsBoundByIndex[0].tap()
+        app.navigationBars.buttons["Back"].tap()
+
+        // Dismiss results view
+        app.swipeDown(velocity: XCUIGestureVelocity.fast)
+    }
+
+    func testNumberExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let numberExampleButton = app.collectionViews.buttons["Number Example"]
+
+        // Open questionnaire
+        numberExampleButton.tap()
+
+        // Fill in integer field
+        let integerField = app.textFields.element
+        integerField.tap()
+        integerField.typeText("1")
+        app.buttons["Next"].tap()
+
+        // Fill in decimal field
+        let decimalField = app.textFields.element
+        decimalField.tap()
+        decimalField.typeText("1.5")
+        app.buttons["Next"].tap()
+
+        // Fill in quantity field
+        let quantityField = app.textFields.element
+        quantityField.tap()
+        quantityField.typeText("2.5")
+
+        // Finish survey
+        let doneButton = app.buttons.matching(identifier: "Done").element(boundBy: 1)
+        doneButton.tap()
+
+        // Open context menu and view results
+        numberExampleButton.press(forDuration: 1.0)
+        app.collectionViews.buttons["View Responses"].tap()
+
+        // Check results
+        let buttonsInResultView = app.collectionViews.allElementsBoundByIndex[1].buttons
+        XCTAssertEqual(buttonsInResultView.count, 1)
+        buttonsInResultView.allElementsBoundByIndex[0].tap()
+        app.navigationBars.buttons["Back"].tap()
+
+        // Dismiss results view
+        app.swipeDown(velocity: XCUIGestureVelocity.fast)
+
+    }
+
+    func textValidationExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let textValidationExampleButton = app.collectionViews.buttons["Text Validation Example"]
+
+        // Open questionnaire
+        textValidationExampleButton.tap()
+
+        // Fill in email
+        let emailField = app.textFields.element
+        emailField.tap()
+        emailField.typeText("vishnu@cardinalkit.org")
+
+        // Finish survey
+        let doneButton = app.buttons.matching(identifier: "Done").element(boundBy: 1)
+        doneButton.tap()
+
+        // Open context menu and view results
+        textValidationExampleButton.press(forDuration: 1.0)
+        app.collectionViews.buttons["View Responses"].tap()
+
+        // Check results
+        let buttonsInResultView = app.collectionViews.allElementsBoundByIndex[1].buttons
+        XCTAssertEqual(buttonsInResultView.count, 1)
+        buttonsInResultView.allElementsBoundByIndex[0].tap()
+        app.navigationBars.buttons["Back"].tap()
+
+        // Dismiss results view
+        app.swipeDown(velocity: XCUIGestureVelocity.fast)
+
     }
 }
