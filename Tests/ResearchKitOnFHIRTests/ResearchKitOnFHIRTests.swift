aa6e890ab22d860d61ecdee887015de4492dfd58
Vishnu Ravi <vishnu.ravi@gmail.com>
2022-09-17 10:57:56 -0400

    Adds unit tests for ResearchKit to FHIR conversion

--- Tests/ResearchKitOnFHIRTests/ResearchKitOnFHIRTests.swift
+++ Tests/ResearchKitOnFHIRTests/ResearchKitOnFHIRTests.swift
@@ -12,9 +12,28 @@ import XCTest
 
 
 final class ResearchKitOnFHIRTests: XCTestCase {
-    func testFHIRToResearchKit() throws {
+    func testCreateORKNavigableOrderedTask() throws {
         let orknavigableOrderedTask = try ORKNavigableOrderedTask(questionnaire: Questionnaire.skipLogicExample)
-        
         XCTAssert(!orknavigableOrderedTask.steps.isEmpty)
     }
+
+    func testCreateNavigationRule() throws {
+        // The skip logic questionnaire has a skip navigation rule on the second step
+        let orknavigableOrderedTask = try ORKNavigableOrderedTask(questionnaire: Questionnaire.skipLogicExample)
+        let secondStepId = try XCTUnwrap(Questionnaire.skipLogicExample.item?[1].linkId.value?.string)
+        XCTAssertNotNil(orknavigableOrderedTask.skipNavigationRule(forStepIdentifier: secondStepId))
+    }
+
+    func testConvertQuestionnaireItemToORKSteps() throws {
+        let title = Questionnaire.numberExample.title?.value?.string ?? "title"
+        let steps = Questionnaire.numberExample.item?.fhirQuestionnaireItemsToORKSteps(title: title, valueSets: [])
+        let unwrappedSteps = try XCTUnwrap(steps)
+        XCTAssertEqual(unwrappedSteps.count, 3)
+    }
+
+    func testGetContainedValueSets() throws {
+        let valueSets = Questionnaire.containedValueSetExample.getContainedValueSets()
+        XCTAssertEqual(valueSets.count, 1)
+    }
+
 }
