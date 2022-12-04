07d9a240d732a4a575158171680bc980c142162f
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2022-12-03 23:02:35 -0800

    Use Accent Color or Tint Color for ORKTaskViewController (#51)

--- Example/ExampleApp/Questionnaire/ORKOrderedTaskView.swift
+++ Example/ExampleApp/Questionnaire/ORKOrderedTaskView.swift
@@ -13,6 +13,7 @@ import UIKit
 
 struct ORKOrderedTaskView: UIViewControllerRepresentable {
     private let tasks: ORKOrderedTask
+    private let tintColor: Color
     // We need to have a non-weak reference here to keep the retain count of the delegate above 0.
     private var delegate: ORKTaskViewControllerDelegate
     
@@ -20,17 +21,25 @@ struct ORKOrderedTaskView: UIViewControllerRepresentable {
     /// - Parameters:
     ///   - tasks: The `ORKOrderedTask` that should be displayed by the `ORKTaskViewController`
     ///   - delegate: An `ORKTaskViewControllerDelegate` that handles delegate calls from the `ORKTaskViewController`. If no  view controller delegate is provided the view uses an instance of `CKUploadFHIRTaskViewControllerDelegate`.
-    init(tasks: ORKOrderedTask, delegate: ORKTaskViewControllerDelegate) {
+    init(
+        tasks: ORKOrderedTask,
+        delegate: ORKTaskViewControllerDelegate,
+        tintColor: Color = Color(UIColor(named: "AccentColor") ?? .tintColor)
+    ) {
         self.tasks = tasks
+        self.tintColor = tintColor
         self.delegate = delegate
     }
     
     
-    func updateUIViewController(_ uiViewController: ORKTaskViewController, context: Context) {}
+    func updateUIViewController(_ uiViewController: ORKTaskViewController, context: Context) {
+        uiViewController.view.tintColor = UIColor(tintColor)
+    }
     
     func makeUIViewController(context: Context) -> ORKTaskViewController {
         // Create a new instance of the view controller and pass in the assigned delegate.
         let viewController = ORKTaskViewController(task: tasks, taskRun: nil)
+        viewController.view.tintColor = UIColor(tintColor)
         viewController.delegate = delegate
         return viewController
     }
