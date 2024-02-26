a95ca90b96cfd51d623baa13b8155a28bea59181
Marek Galik <mgalik@goldmann.sk>
2024-02-26 08:50:41 +0100

    MR issue - Concrete inits

--- Sources/ResearchKitOnFHIR/ResearchKitToFHIR/ORKTaskResult+FHIR.swift
+++ Sources/ResearchKitOnFHIR/ResearchKitToFHIR/ORKTaskResult+FHIR.swift
@@ -131,7 +131,7 @@ extension ORKTaskResult {
             if let valueCodingString = answer as? String, let valueCoding = ValueCoding(rawValue: valueCodingString) {
                 let coding = Coding(
                     code: FHIRPrimitive(FHIRString(valueCoding.code)),
-                    display: valueCoding.display.map { .init(.init($0)) },
+                    display: valueCoding.display.map { FHIRPrimitive(FHIRString($0)) },
                     system: FHIRPrimitive(FHIRURI(stringLiteral: valueCoding.system))
                 )
                 responses += [.coding(coding)]
