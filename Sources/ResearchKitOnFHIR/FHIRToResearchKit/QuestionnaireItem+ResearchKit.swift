80fc97d272d02f62aff8d5d9811fc55952d60001
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2025-07-21 20:06:56 +0200

    Even more printing

--- Sources/ResearchKitOnFHIR/FHIRToResearchKit/QuestionnaireItem+ResearchKit.swift
+++ Sources/ResearchKitOnFHIR/FHIRToResearchKit/QuestionnaireItem+ResearchKit.swift
@@ -290,6 +290,7 @@ extension QuestionnaireItem {
                     continue
                 }
                 let valueCoding = ValueCoding(code: code, system: system, display: display)
+                print("Coding:", valueCoding.rawValue)
                 let choice = ORKTextChoice(text: display, value: valueCoding.rawValue as NSSecureCoding & NSCopying & NSObjectProtocol)
                 choices.append(choice)
             }
@@ -308,6 +309,7 @@ extension QuestionnaireItem {
                     continue
                 }
                 let valueCoding = ValueCoding(code: code, system: system, display: display)
+                print("Coding:", valueCoding.rawValue)
                 let choice = ORKTextChoice(text: display, value: valueCoding.rawValue as NSSecureCoding & NSCopying & NSObjectProtocol)
                 choices.append(choice)
             }
