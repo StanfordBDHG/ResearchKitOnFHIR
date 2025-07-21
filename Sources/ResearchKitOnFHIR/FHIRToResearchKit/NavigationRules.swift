55624cb724e5b225f5cd6e27a9e82a1cfde54d76
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2025-07-21 20:32:47 +0200

    Remove stepIdentifier

--- Sources/ResearchKitOnFHIR/FHIRToResearchKit/NavigationRules.swift
+++ Sources/ResearchKitOnFHIR/FHIRToResearchKit/NavigationRules.swift
@@ -50,11 +50,7 @@ extension QuestionnaireItemEnableWhen {
               let fhirOperator = `operator`.value else {
             return nil
         }
-        let formSteps = task.steps.compactMap { $0 as? ORKFormStep }
-        let stepIdentifier = formSteps
-            .first { $0.formItems?.contains(where: { $0.identifier == enableQuestionId }) != nil }?
-            .identifier
-        let resultSelector = ORKResultSelector(stepIdentifier: stepIdentifier, resultIdentifier: enableQuestionId)
+        let resultSelector = ORKResultSelector(resultIdentifier: enableQuestionId)
         switch answer {
         case .coding(let coding):
             return try coding.predicate(with: resultSelector, operator: fhirOperator)
