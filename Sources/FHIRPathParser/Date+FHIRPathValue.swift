dfb5fc8241a0e8f9198e3f402a1e86fd90469643
Lukas Kollmer <hey@lukaskollmer.de>
2026-02-18 13:36:51 +0100

    FHIRPath: DateComponents support

--- Sources/FHIRPathParser/Date+FHIRPathValue.swift
+++ Sources/FHIRPathParser/Date+FHIRPathValue.swift
@@ -11,10 +11,7 @@ import Foundation
 
 extension Date: _FHIRPathValue {
     public static func evaluate(_ expression: FHIRPathParser.ExpressionContext) throws -> Date {
-        let visitor = DateExpressionEvaluation()
-        let result = expression.accept(visitor)
-
-        switch result {
+        switch expression.accept(DateExpressionEvaluation()) {
         case .none:
             throw DateExpressionError.internalError
         case let .failure(error):
@@ -26,10 +23,31 @@ extension Date: _FHIRPathValue {
                 guard let date = Calendar.current.date(from: components) else {
                     throw DateExpressionError.failedDateOperation(reason: .componentsDoNotFormValidDate)
                 }
-
                 return date
             case let .date(date):
                 return date
+            }
+        }
+    }
+}
+
+
+extension DateComponents: _FHIRPathValue {
+    public static func evaluate(_ expression: FHIRPathParser.ExpressionContext) throws -> DateComponents {
+        switch expression.accept(DateExpressionEvaluation()) {
+        case .none:
+            throw DateExpressionError.internalError
+        case let .failure(error):
+            throw error
+        case let .success(value):
+            switch value {
+            case let .components(components):
+                return components
+            case let .date(date):
+                return Calendar.current.dateComponents(
+                    [.timeZone, .year, .month, .day, .hour, .minute, .second],
+                    from: date
+                )
             }
         }
     }
